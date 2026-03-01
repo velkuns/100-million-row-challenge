887eb8fd5cc3b46d71b83b796c4baa602ee64dad
velkuns <velkuns@users.noreply.github.com>
2026-03-01 10:28:31 +0100

    velkuns-singlethread

--- app/Parser.php
+++ app/Parser.php
@@ -8,6 +8,20 @@ final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
     {
-        throw new Exception('TODO');
+        $csv = new \SplFileObject($inputPath, 'r');
+        $csv->setFlags(\SplFileObject::READ_CSV | \SplFileObject::SKIP_EMPTY | \SplFileObject::DROP_NEW_LINE | \SplFileObject::READ_AHEAD);
+
+        $data = [];
+        foreach ($csv as [$url, $date]) {
+            $url  = \substr($url, 19);
+            $date = \substr($date, 0, 10);
+            $data[$url][$date] = ($data[$url][$date] ?? 0) + 1;
+        }
+
+        foreach ($data as &$items) {
+            \ksort($items);
+        }
+
+        \file_put_contents($outputPath, \json_encode($data, flags: \JSON_PRETTY_PRINT));
     }
 }
